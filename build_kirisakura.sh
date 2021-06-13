909679223b354c96eca9c144ebdab211abba60e8
vinz <vinz@gmail.com>
2021-06-13 22:44:52 +0900

    ./build

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -13,10 +13,10 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=/media/miles/Storage/CLANG_TC/tc-build/install/bin
+export CLANG_PATH=/home/darkidz/linux-x86/clang-r416183b/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/darkidz/android-ndk-r18b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 
 echo
 echo "Set DEFCONFIG"
