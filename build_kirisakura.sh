5ff8aa046c98bed62f7f11819f1d38c862583800
vinz <vinz@gmail.com>
2021-06-21 14:02:02 +0900

    use latest clang and cross compile

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -4,8 +4,6 @@ echo
 echo "Clean Build Directory"
 echo 
 
-make clean && make mrproper
-
 echo
 echo "Issue Build Commands"
 echo
@@ -13,10 +11,10 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=/home/darkidz/linux-x86/clang-r416183b/bin
+export CLANG_PATH=/home/darkidz/linux-x86/clang-r416183c/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=/home/darkidz/android-ndk-r18b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/darkidz/android-ndk-r22b/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-
 
 echo
 echo "Set DEFCONFIG"
